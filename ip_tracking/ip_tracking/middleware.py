def3d5af2cafd836cd172199581972357b90c9d1
Yohannes Dawit <yohannesabreham08@gmail.com>
2025-09-02 00:15:44 +0300

    Task 2: IP Geolocation Analytics

--- ip_tracking/ip_tracking/middleware.py
+++ ip_tracking/ip_tracking/middleware.py
@@ -1,9 +1,11 @@
-from .models import RequestLog
+from django.http import HttpResponseForbidden
 from django.utils.timezone import now
+from .models import RequestLog, BlockedIP
+
 
 class IPLoggingMiddleware:
     """
-    Middleware to log client IP address, timestamp, and request path.
+    Middleware to log requests and block blacklisted IPs.
     """
 
     def __init__(self, get_response):
@@ -13,11 +15,15 @@ class IPLoggingMiddleware:
         ip = request.META.get("REMOTE_ADDR")
 
         if ip:
+            # Block if IP is in blacklist
+            if BlockedIP.objects.filter(ip_address=ip).exists():
+                return HttpResponseForbidden("Your IP has been blocked.")
+
+            # Otherwise log request
             RequestLog.objects.create(
                 ip_address=ip,
                 timestamp=now(),
                 path=request.path,
             )
 
-        response = self.get_response(request)
-        return response
+        return self.get_response(request)
